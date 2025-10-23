304f875be7e4a1eee747eb3cbc63648646f01644
Filippe Melo <filippemelor@gmail.com>
2025-10-23 17:53:56 -0300

    Definindo o relacionamento entre as entidades.

--- ApiCatalogo/ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -1,8 +1,12 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string Nome { get; set; } = null!;
     public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto> Produtos { get; set; } = new Collection<Produto>();
 }
