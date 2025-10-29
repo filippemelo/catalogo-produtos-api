9394cf5292f014e21c4e5dcca47614a8b2e456e5
Filippe Melo <filippemelor@gmail.com>
2025-10-28 22:56:20 -0300

    Criando métodos Post, Put e Delete de ProdutosController.

--- ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -27,14 +27,51 @@ public class ProdutosController : ControllerBase
         return produtos;
     }
 
-    [HttpGet("{id:int}")]
+    [HttpGet("{id:int}", Name = "ObterProduto")]
     public ActionResult<Produto> Get(int id)
     {
-        var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+        var produto = _context.Produtos.AsNoTracking().FirstOrDefault(x => x.ProdutoId == id);
         if (produto is null)
-            return NotFound();
+            return NotFound("Produto não encontrado.");
 
         return produto;
     }
 
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.ProdutoId)
+            return BadRequest();
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+
+        if (produto is null)
+            return NotFound("Produto não encontrado.");
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
 }
