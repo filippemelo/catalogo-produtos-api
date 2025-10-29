d90af592569bf50b35817056c93d88db2bf9fb5a
Filippe Melo <filippemelor@gmail.com>
2025-10-28 22:37:05 -0300

    Criando ProdutosController e métodos Get.

---
+++ ApiCatalogo/ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.AsNoTracking().ToList();
+
+        if (produtos is null)
+            return NotFound("Produtos não encontrados.");
+
+        return produtos;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+        if (produto is null)
+            return NotFound();
+
+        return produto;
+    }
+
+}
